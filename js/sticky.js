80cd1099627a1e4b5bada88d2394c4b9872f75c2
Maxime van Steenberghe <maxime.vansteenberghe@gmail.com>
2017-07-20 17:54:58 +0200

    Sticky method draft : le menu de gauche reste fixé au top lorsqu'on scroll down

---
+++ js/sticky.js
@@ -0,0 +1,14 @@
+// Contact Form Scripts
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var distanceFromTop = $(document).scrollTop();
+        if (distanceFromTop >= $('#header').height())
+        {
+            $('#sticky').fadeIn(400).addClass('fixed');
+        }
+        else
+        {
+            $('#sticky').fadeOut(400).removeClass('fixed');
+        }
+    });
+});
