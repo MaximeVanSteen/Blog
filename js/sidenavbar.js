1bb690c3e98ac1998831331299f4c9f1b33a39be
Maxime van Steenberghe <maxime.vansteenberghe@gmail.com>
2017-07-26 12:33:59 +0200

    sidenavbar stay open after selecting an item + scroll smoothly to the selected item. Only problem left : the selected item has a background color that is not correct

--- js/sidenavbar.js
+++ js/sidenavbar.js
@@ -1,5 +1,6 @@
-// Fix Navbar to the top of the window when scrolling below header
+
 $(document).ready(function() {
+    // Fix Navbar to the top of the window when scrolling below header
     $(window).scroll(function() {
         var distanceFromTop = $(document).scrollTop();
         if (distanceFromTop >= $('#header').height())
@@ -11,15 +12,22 @@ $(document).ready(function() {
             $('#sticky').fadeIn(400).removeClass('fixed');
         }
     });
+    // Prevent dropdown from closing after selecting an item
+    $('.dropdown-menu').on({
+        "click":function(e){
+            e.stopPropagation();
+        }
+    });
+    // scroll smoothly to the selected item
+    $('.sidenavbar-button').on('click', function(event){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+    });
 });
 
-// Smooth scrolling using the side navbar
-$(document).on('click', '.sidenavbar-button', function(event){
-    event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-});
+
 
 
